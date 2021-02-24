df055bf31f88dd38c774dfa7ba4753a900b53cbd
Jenny <han.jennyjh@outlook.com>
2021-02-24 20:30:51 +0000

    Got rid of sum() in add def

--- arithmetic.py
+++ arithmetic.py
@@ -3,7 +3,7 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-    return sum(num1,num2)
+    return num1 + num2 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
