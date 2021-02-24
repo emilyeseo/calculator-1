0e25998eaa86d7c202aee21f225a7d19179fabec
Jenny <han.jennyjh@outlook.com>
2021-02-24 20:33:40 +0000

    Changed num2 places with num1

--- arithmetic.py
+++ arithmetic.py
@@ -7,7 +7,7 @@ def add(num1, num2):
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-    return num2 - num1 
+    return num1 - num2 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
@@ -15,7 +15,7 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-    return num2 / num1
+    return num1 / num2
 
 def square(num1):
     """Return the square of the input."""
